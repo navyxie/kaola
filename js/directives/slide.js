5f17e2f039341c82569fc706ec29305d275004db
navyxie <navyxie2010@gmail.com>
2014-12-19 01:01:28 +0800

    add interval

--- js/directives/slide.js
+++ js/directives/slide.js
@@ -1,14 +1,36 @@
 'use strict';
 angular.module('kaolaApp.slideDirective',[])
-.directive('slider',function(){
+.directive('slider',['$interval',function($interval){
 	return {
 		restrict: 'ECA',
+		scope:{
+			index:"@slideIndex",
+			name:"@slideName"
+		},
 		link:function(scope, element, attr){
-			// console.log(angular.element('ngSlide'));
+			scope.index = scope.index  || 0;
+			// console.log(scope.name);
 			var children = element.children();
-			angular.forEach(children,function(item,key){
-				console.log(angular.element(item));
-			});
+			var len = children.length;			
+			function updateDom(index){
+				angular.forEach(children,function(item,key){
+					if(index == key){
+						angular.element(item).css({display:'block',opacity:1});
+					}else{
+						angular.element(item).css({display:'none',opacity:0});
+					}
+				});
+			}
+			updateDom(scope.index);
+			$interval(function(){
+				++scope.index;
+				if(scope.index >= len){
+					scope.index = 0;
+				}
+				updateDom(scope.index);
+			},3000);
 		}
 	}
+}]).controller('sliderCtrl',function($scope){
+	$scope.name = 'navy';
 });
