2f73d746713df838a1bf6be22f209f67e3287e59
navy <navyxie2010@gmail.com>
2014-12-19 19:56:48 +0800

    增加滚动测试

--- views/index/view.js
+++ views/index/view.js
@@ -1,10 +1,34 @@
 'use strict';
-angular.module('kaolaApp.IndexView',['ngRoute','kaolaApp.slideDirective'])
+angular.module('kaolaApp.IndexView',['ngRoute','kaolaApp.slideDirective','duScroll'])
 .config(['$routeProvider',function($routeProvider){
 	$routeProvider.when('/',{
 		templateUrl:"views/index/view.html",
 		controller:'indexCtrl'
 	});
-}]).controller('indexCtrl',[function(){
-
-}]);
+}]).controller('indexCtrl',['$scope','$window','$document','$interval','$timeout',function($scope,$window,$document,$interval,$timeout){
+	var i = 0;
+	var defaultTop = 0;
+	var flag = false;
+	var direction = null;
+	var scrollFn = function(){
+		if(!flag && defaultTop != $document.scrollTop()){
+			flag = true;
+			console.log(defaultTop,$document.scrollTop())
+			if(defaultTop > $document.scrollTop()){
+				direction = 'up';
+				--i;
+			}else{
+				++i;
+				direction = 'down';
+			}			
+			console.log(direction);
+			console.log(i);
+			$document.scrollTop(200*(++i));
+			$timeout(function(){
+				defaultTop = $document.scrollTop();
+				flag = false;
+			},350);			
+		}
+	}
+	$document.on('scroll',scrollFn);
+}]).value('duScrollOffset', 30);
